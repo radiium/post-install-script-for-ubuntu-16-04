104d9e2685354be78b1f979c4408fd08ec4b8af5
remi.poncet <remi.poncet@openmailbox.org>
2016-09-19 21:05:30 +0200

    First commit: add scripts

---
+++ post-install.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# Authors     : radiium <radiium@openmailbox.org>
+# Description : A post-installation bash script for Ubuntu
+# version     : 0.1
+# ----------------------------------------------------------------------------
+
+# tab width
+tabs 4
+clear
+
+#----- Import Functions -----#
+
+dir="$(dirname "$0")"
+
+. $dir/functions/check
+. $dir/functions/configure
+. $dir/functions/update
+. $dir/functions/codecs
+. $dir/functions/softwares
+. $dir/functions/cleanup
+
+
+# . $dir/functions/development
+# . $dir/functions/favs
+# . $dir/functions/gnome
+# . $dir/functions/thirdparty
+# . $dir/functions/utilities
+
+#----- Fancy Messages -----#
+show_error(){
+echo -e "\033[1;31m$@\033[m" 1>&2
+}
+show_info(){
+echo -e "\033[1;32m$@\033[0m"
+}
+show_warning(){
+echo -e "\033[1;33m$@\033[0m"
+}
+show_question(){
+echo -e "\033[1;34m$@\033[0m"
+}
+show_success(){
+echo -e "\033[1;35m$@\033[0m"
+}
+show_header(){
+echo -e "\033[1;36m$@\033[0m"
+}
+show_listitem(){
+echo -e "\033[0;37m$@\033[0m"
+}
+
+#----- Main Menu -----#
+function main {
+    MAIN=$(whiptail \
+        --notags \
+        --title "Ubuntu Post-Install Script" \
+        --menu "\nWhat would you like to do?" \
+        --cancel-button "Quit" \
+        0 0 0 \
+        configure   "Configure system" \
+        update      "Perform system update" \
+        codecs      "Install codecs" \
+        softwares   "Install preferred software" \
+        cleanup     "Cleanup the system" \
+        3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        $MAIN
+    else
+        quit
+    fi
+}
+
+# Quit
+function quit {
+    if (whiptail --title "Quit" --yesno "Are you sure you want quit?" 10 60) then
+        echo "Exiting..."
+        show_info 'Thanks for using!'
+        exit 99
+    else
+        main
+    fi
+}
+
+#RUN
+check_dependencies
+while :
+do
+  main
+done
+
+#END OF SCRIPT
